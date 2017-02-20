4292ab1db794afc556af0df24924434f9f3c40a6
Shizuoko <daduhka.2009@gmail.com>
2017-02-20 10:38:37 +0200

    Map loader

--- src/Map.kt
+++ src/Map.kt
@@ -1,9 +1,28 @@
 import java.io.File
+import java.util.*
+import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by Shizuoko on 17.02.2017.
  */
+
 fun LoadMap()
 {
-    val Map: String = File("assets/map.txt").readText(charset = Charsets.UTF_8)
+    val Size = 40
+
+    val MapFile: String = File("assets/map.txt").readText(charset = Charsets.UTF_8)
+    var Map = Array(Size, { IntArray(Size) })
+
+    var MapFileIterator = 0
+
+    for (Yindex: Int in 0..39)
+    {
+        for (Xindex: Int in 0..39)
+        {
+            var MapCharacter = MapFile.get(MapFileIterator).toInt()
+            Map[Yindex][Xindex] = MapCharacter
+            MapFileIterator++
+        }
+    }
 }
