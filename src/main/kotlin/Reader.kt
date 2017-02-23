bc96d4ffa2d2896fc7a577c41120c499d2e3ee89
Shizuoko <daduhka.2009@gmail.com>
2017-02-23 17:04:09 +0200

    grandle version

---
+++ src/main/kotlin/Reader.kt
@@ -0,0 +1,12 @@
+package reader
+
+import com.fasterxml.jackson.core.JsonFactory
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+
+/**
+ * Created by Harmitage on 19.08.2016.
+ */
+object Reader {
+    val JSON = jacksonObjectMapper()
+    val JSONFactory = JsonFactory()
+}
